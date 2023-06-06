838d68fa0cc0e7cdb225967edeb77345e7b5bdb4
Maimoon Ahmad <67364162+Maimoon27@users.noreply.github.com>
2023-06-07 00:55:21 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"
+export var typingEffect = new Typed('.multitext', {
+    strings: ['Content Writer','Web Developer','Product Manager'],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500
+    
+});
